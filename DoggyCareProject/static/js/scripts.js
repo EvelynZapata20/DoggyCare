9831fbe21081b115080033db222d255da48b98f7
Evelyn <eazapatat@eafit.edu.co>
2023-11-15 17:03:00 -0500

    tooltip for the owner to see the vet's rating and responsive design of my tabs

--- DoggyCareProject/static/js/scripts.js
+++ DoggyCareProject/static/js/scripts.js
@@ -47,3 +47,40 @@ starRating.forEach(input => {
         });
     });
 });
+
+
+
+// Event for the tooltip for the vet rating
+document.addEventListener("DOMContentLoaded", function () {
+    const vetInfoList = document.getElementsByClassName('vet-info');
+
+    const tooltip = document.createElement('div');
+    tooltip.style.position = 'absolute';
+    tooltip.style.visibility = 'hidden';
+    tooltip.style.backgroundColor = '#fff'; 
+    tooltip.style.padding = '5px';
+    tooltip.style.border = '1px solid rgb(182, 149, 105)';
+    tooltip.style.borderRadius = '5px';
+    tooltip.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+    document.body.appendChild(tooltip);
+
+    for (const vetInfo of vetInfoList) {
+        vetInfo.addEventListener('mouseover', function (event) {
+            const name = this.getAttribute('data-name');
+            const rating = this.getAttribute('data-rating');
+
+            tooltip.innerHTML = `<p>Rating: ${rating}<span style="color: yellow;">&#9733;</span></p>`;
+
+            tooltip.style.top = (event.pageY + 10) + 'px';
+            tooltip.style.left = (event.pageX + 10) + 'px';
+
+            tooltip.style.visibility = 'visible';
+            this.style.textDecoration = 'underline';
+        });
+
+        vetInfo.addEventListener('mouseout', function () {
+            tooltip.style.visibility = 'hidden';
+            this.style.textDecoration = 'none';
+        });
+    }
+});
